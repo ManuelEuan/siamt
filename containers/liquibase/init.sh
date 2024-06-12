1bac3feb6803f5ffe7cc874b75582dc065a777e5
Gaspar Uc <rohesmen@gmail.com>
2024-06-12 10:52:37 -0600

    config liquibase

--- containers/liquibase/init.sh
+++ containers/liquibase/init.sh
@@ -2,7 +2,7 @@
 
 liquibase \
   --log-level=INFO \
-  --url="jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_DBNAME" \
+  --url="jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_DBNAMEA" \
   --classpath=/liquibase/changelog --changeLogFile=dbchangelog.xml \
   --username=$DB_USER \
   --password=$DB_PASS update
